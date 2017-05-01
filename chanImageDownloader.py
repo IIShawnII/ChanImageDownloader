cf6f4e1ba2aef2c68a815aa8f6834fd020ba1889
Shariar Emami <sh.emami65@gmail.com>
2017-05-01 17:24:20 -0400

    added gitignore and python styling
    changed function and varibales names to python suggested style. added
    download location of files and IDE folder to ignore list

--- chanImageDownloader.py
+++ chanImageDownloader.py
@@ -15,130 +15,140 @@ Optional arguments:
   -d          show extra print outs.
 """
 
-import requests, os, json, sys, argparse
+import os
+import sys
+import requests
+import argparse
 
-#all threads in a board
+# all threads in a board
 URL_THREADS = "https://8ch.net/{0}/threads.json"
-#all posts in a thread
+# all posts in a thread
 URL_POSTS = "https://8ch.net/{0}/res/{1}.json"
-#all boards
+# all boards
 URL_BOARDS = "https://8ch.net/boards.json"
-#old image location
+# old image location
 URL_OLD_IMAGE = "https://media.8ch.net/{0}/src/"
-#new image location
+# new image location
 URL_NEW_IMAGE = "https://media.8ch.net/file_store/"
 
 debug = False
 
-def toJson( response):
+
+def to_json(response):
     try:
-        json = response.json()
-        return json
+        decode = response.json()
+        return decode
     except Exception as ex:
-        print( 'Problem decoding json', file=sys.stderr)
-        print( ex, file=sys.stderr)
+        print('Problem decoding response', file=sys.stderr)
+        print(ex, file=sys.stderr)
         return False
 
-def getURL( url, streaming=False):
+
+def get_url(url, streaming=False):
     try:
-        response = requests.get( url, stream=streaming)
+        response = requests.get(url, stream=streaming)
         response.raise_for_status()
         return response
     except Exception as ex:
-        print( 'Problem when getting: {0}'.format(url), file=sys.stderr)
-        print( ex, file=sys.stderr)
+        print('Problem when getting: {0}'.format(url), file=sys.stderr)
+        print(ex, file=sys.stderr)
     return False
 
 
-def getThreadsInBoard( board):
-    threads = getURL( URL_THREADS.format(board))
+def get_threads_in_board(board):
+    threads = get_url(URL_THREADS.format(board))
     if threads:
-        return [ thread['no'] for page in threads.json() for thread in page[ 'threads']]
+        return [thread['no'] for page in to_json(threads) for thread in page['threads']]
     return False
 
 
-def getFilesInThread( board, thread):
-    posts = getURL( URL_POSTS.format( board, thread))
-    
+def get_files_in_thread(board, thread):
+    posts = get_url(URL_POSTS.format(board, thread))
+
     if not posts:
         return False
 
-    postsJson = toJson( posts)
-    
-    if not postsJson:
+    posts_json = to_json(posts)
+
+    if not posts_json:
         return False
 
-    def getFileDetails( post):
-        ext = post.get('ext', post)
-        name = post.get('filename', post) + ext
-        address = post.get('tim', post) + ext
-        return {"name":name, "address":address}
+    def get_file_details(post_dic):
+        ext = post_dic.get('ext', None)
+        name = post_dic.get('filename', None) + ext
+        address = post_dic.get('tim', None) + ext
+        return {"name": name, "address": address}
 
     files = []
-    for post in postsJson['posts']:
+    for post in posts_json['posts']:
         if post.get('ext'):
-            files.append( getFileDetails( post))
+            files.append(get_file_details(post))
         if post.get('extra_files'):
             for extra_file in post.get('extra_files'):
-                files.append( getFileDetails( extra_file))
+                files.append(get_file_details(extra_file))
 
     return files
 
 
-def getFile( folder, board, thread, file):
-    filepath = folder + file['name']
-    if not os.path.exists( filepath):
-        response = getURL( URL_NEW_IMAGE + file['address'], True);
+def get_file(folder, board, file):
+    file_path = folder + file['name']
+    if not os.path.exists(file_path):
+        response = get_url(URL_NEW_IMAGE + file['address'], True)
         if not response:
-            response = getURL( URL_OLD_IMAGE.format( board) + file['address'], True);
-        with open( filepath, 'wb') as f:
-            for chunk in r.iter_content( chunk_size=1024):
+            response = get_url(URL_OLD_IMAGE.format(board) + file['address'], True)
+        with open(file_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=1024):
                 if chunk:
-                    f.write( chunk)
+                    f.write(chunk)
                     f.flush()
-        r.close()
         return True
     return False
 
-def processThread( board, thread):
-    print( 'Board: {0}, Thread: {1:>4} getting image list'.format( board, str(  thread)))
-    files = getFilesInThread( board, thread)
+
+def process_thread(board, thread):
+    print('Board: {0}, Thread: {1:>4} getting image list'.format(board, str(thread)))
+    files = get_files_in_thread(board, thread)
     if not files:
-        print( 'Board: {0}, Thread: {1} No image or thread found'.format( board, str( thread)))
+        print('Board: {0}, Thread: {1} No image or thread found'.format(board, str(thread)))
         return
-    
-    folder = './8ch/'+ board +'/'+ str(thread) + '/'
-    if not os.path.exists( folder):
-        os.makedirs( folder)
+
+    folder = './8ch/' + board + '/' + str(thread) + '/'
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     for file in files:
-        if getFile( folder, board, thread, file):
-            print( 'GET [Board: {0}, Thread: {1}, File: {2}]'.format( board, thread, file['name']))
+        if get_file(folder, board, file):
+            print('GET [Board: {0}, Thread: {1}, File: {2}]'.format(board, thread, file['name']))
         elif debug:
-            print( 'Exists [Board: {0}, Thread: {1}, File: {2}]'.format( board, thread, file['name']))
+            print('Exists [Board: {0}, Thread: {1}, File: {2}]'.format(board, thread, file['name']))
 
 
-def processBoard( board):
-    print( 'Board: {0} getting thread list'.format( board))
-    threads = getThreadsInBoard( board)
+def process_board(board):
+    print('Board: {0} getting thread list'.format(board))
+    threads = get_threads_in_board(board)
     if not threads:
         return
     if debug:
-        print( threads)
+        print(threads)
     for thread in threads:
-        processThread( board, thread)
+        process_thread(board, thread)
 
-def configArgParse():
-    parser = argparse.ArgumentParser( description='8chan Downloader')
-    parser.add_argument('-b', metavar='Board', dest='board', type=str, nargs=1, help='*Required arg. name of the board to downlaod everything within, unless thread number is provided.', required=True)
-    parser.add_argument('-t', metavar='Thread', dest='thread', type=int, nargs=1, help='Optional arg. thread number. if present only the thread provided will be downloaded.')
+
+def config_argparse():
+    parser = argparse.ArgumentParser(description='8chan Downloader')
+    parser.add_argument('-b', metavar='Board', dest='board', type=str, nargs=1,
+                        help='*Required arg. name of the board to downlaod everything within, unless thread number is provided.',
+                        required=True)
+    parser.add_argument('-t', metavar='Thread', dest='thread', type=int, nargs=1,
+                        help='Optional arg. thread number. if present only the thread provided will be downloaded.')
     parser.add_argument('-d', dest='debug', action='store_true', help='Optional arg. show extra print outs.')
     return parser
 
+
 if __name__ == '__main__':
-    args = configArgParse().parse_args()
+    args = config_argparse().parse_args()
     if args.debug:
         debug = True
     if args.thread:
-        processThread( args.board[0], args.thread[0])
+        process_thread(args.board[0], args.thread[0])
     elif args.board:
-        processBoard( args.board[0])
+        process_board(args.board[0])
